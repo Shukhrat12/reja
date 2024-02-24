230713a7c1822e7efad0a4db622add3029f889fe
Shukhfayz <shukhfayz@gmail.com>
2024-02-23 21:50:04 -0500

    G-TASK

--- train.js
+++ train.js
@@ -1,30 +1,32 @@
-/*F-TASK: 
+/*G-TASK: 
 
-Shunday findDoublers function tuzing, unga faqat bitta string argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
-MASALAN: getReverse("hello") return true return qiladi
+Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+MASALAN: getHighestIndex([5, 21, 12, 22, 8]) return qiladi 1 sonini.
 
 @MITASK
 */
 
-function findDuplicates(str) {
-  let count = 0;
-  const result = [];
-  for (let i = 0; i < str.length; i++) {
-    for (let j = i+1; j < str.length; j++) {
-      if(str[i] === str[j]) {
-        count++
-        result.push(str[i])
-        console.log(result)
-      }
+// 1. Shunday function tuzingki unga integerlardan iborat array pass bolsin
+// 2. Bitta yangi max deb nomlangan variable create qilamiz va 0 index dagi array elementini solamiz
+// 3. Yana bitta yangi maxIndex degan variable yaratamiz va boshlang'ich qiymatini 0 ga tenglaymiz
+// 4. Arrayni loop qilamiz
+// 5. Loop qilayotgan har bir array elementini max bilan solishtirib max dan kattami yo'qmi tekshiramiz
+// 6. Agar max dan katta bo'lsa, max ga o'sha array elementini qiymatini tenglaymiz
+// 7. Index ni ya'ni i ni maxIndex ga tenglaymiz
+// 6. Loopdan tashqariga chiqib maxIndex ni return qilamiz
+
+function getHighestIndex(arr) {
+  let max = arr[0];
+  let maxIndex = 0
+  for(let i=0; i<arr.length; i++) {
+    if(max < arr[i]) {
+      max = arr[i]
+      maxIndex = i
     }
   }
-  if (count>0) {
-    return true
-  } else {
-    return false
-  }
-  
+  return maxIndex
 }
 
-const result = findDuplicates("helloo")
-console.log(result)
+const a = [5, 21, 20000, 12, 80000, 9000];
+const maximumIndex = getHighestIndex(a)
+console.log(maximumIndex)
